8a5fe3716b37cf96d2c6a2095c27c9ec4dc73026
Deepak Singh <136564807+deepaksingh9696@users.noreply.github.com>
2024-10-17 23:33:58 +0530

    file uploaded

---
+++ data/index.ts
@@ -0,0 +1,226 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+
+    title:
+      "Deepak Singh - Skilled MERN Stack Developer Specializing in Creating High-Performance, Full-Stack Web Solutions  ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/d1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building my portfolio",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    title: "A Real-Time Chat application ",
+    des: "A real-time chat application allows users to send and receive messages instantly, supporting features like user authentication, private and group chats, and real-time message updates using WebSockets or similar technologies. ",
+    img: "/e1.svg",
+    iconLists: [],
+    link: "/ui.earth.com",
+    githubLink: "https://github.com/deepaksingh9696/Chat-app",
+  },
+  {
+    id: 2,
+    title: "EchoNotes- A Note-App",
+    des: " A Note-App is a user-friendly application designed to help users create, organize, and manage their notes efficiently. It features secure login and registration, allowing users to keep their notes private. EchoNotes provides functionalities like adding, editing, and deleting notes, with a powerful search feature to quickly find specific notes.",
+    img: "/g2.svg",
+    iconLists: [],
+    link: "/ui.yoom.com",
+    githubLink: "https://github.com/deepaksingh9696/EchoNotes",
+  },
+  {
+    id: 3,
+    title: "Job Plateform Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/h2.svg",
+    iconLists: [],
+    link: "/ui.aiimg.com",
+    githubLink: "https://github.com/deepaksingh9696/Career-Compass",
+  },
+  {
+    id: 4,
+    title: "Videos-Gallery-Hub-Website",
+    des: "Videos-Gallery-Hub is a platform designed for organizing, viewing, and sharing videos. Users can upload, categorize, and manage video content easily, creating a personalized gallery for various types of media.",
+    img: "/g4.svg",
+    iconLists: [],
+    link: "/ui.apple.com",
+    githubLink: "https://github.com/deepaksingh9696/Videos-Gallery-Hub",
+  },
+];
+
+
+export const testimonials = [
+  {
+    quote:
+      "The entire process was smooth and effortless thanks to their clear communication and efficient project management They understood our requirements right from the start and delivered a solution that integrated perfectly with our workflow. We were kept informed at every stage, and the delivery was flawless. The team made sure there were no surprises along the way.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Their deep understanding of the technology and industry trends allowed them to provide insights we hadn’t even considered.The level of technical knowledge they brought to the project was unparalleled. They solved complex problems with ease and creativity.Their expertise ensured that the project not only met our current needs but was also scalable and future-proof.",
+    name: "Sophia Richards",
+    title: "Marketing Director at CreativePulse",
+  },
+  {
+    quote:
+      "Whenever we had a question or concern, their support team was just a call away, always eager to help and provide clear, actionable advice.Even after the project was completed, they continued to offer fantastic support, ensuring that everything was running smoothly.It was reassuring to know that help was always available when needed, and they never hesitated to assist with even the smallest issues.",
+    name: "Amit Patel",
+    title: "Product Manager at NextGen Media",
+  },
+  {
+    quote:
+      "They never missed a deadline, and their ability to stay on track gave us confidence throughout the entire project.We could rely on them 100% to deliver what they promised, and they consistently exceeded those promises.Their commitment to reliability made working with them an absolute pleasure. We always knew we were in good hands",
+    name: "David Nguyen",
+    title: "Chief Technology Officer at DataCraft",
+  },
+  {
+    quote:
+      "Their out-of-the-box thinking and creative solutions made a huge difference in the success of our project.They brought fresh, innovative ideas to the table, helping us to stand out and offer something truly unique.We were impressed by their creativity and how they turned our vision into something even better than we had imagined",
+    name: "Lisa Bennett",
+    title: "Founder of Spark Innovations",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer ",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer",
+    desc: "A Software Engineer is responsible for designing, developing, testing, and maintaining software applications.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "MERN Stack Developer",
+    desc: " Design and develop front-end and back-end components of web applications using React.js for the client side and Node.js with Express.js for the server side.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/deepaksingh9696?tab=repositories",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://x.com/DeepakS48250085",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/am-deepak-singh/",
+  },
+];
